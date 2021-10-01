a93651e51874ed8b39a0293a5f92ba6abc03cd54
Akansha Zutshi <90608726+20Akansha@users.noreply.github.com>
2021-10-01 19:26:10 +0530

    Update PRACTICAL-2.py

--- PRACTICAL-2.py
+++ PRACTICAL-2.py
@@ -1,39 +1,38 @@
-def union(criket, badminton):                            
-    L = list(criket)  
+def union(cricket, badminton):                            
+    L = list(cricket)  
     for i in badminton:
-        if i not in criket:
+        if i not in cricket:
             L.append(i)
     return L
 
-
-def intersection(criket,badminton):
+def intersection(cricket,badminton):
     L = []
-    for i in criket:
+    for i in cricket:
         if i in badminton:
             L.append(i) 
     return L
 
-def eiter_or(criket, badminton):
-    u = union(criket, badminton)
-    i = intersection(criket, badminton)
+def neither_or(cricket, badminton):
+    u = union(cricket, badminton)
+    i = intersection(cricket, badminton)
     for value in i:
         u.remove(value)
     return u
 
-def minus(criket, badminton):
-    L = list(criket)
+def minus(cricket, badminton):
+    L = list(cricket)
     for i in badminton:
         if i in L:
             L.remove(i)
     return L
 
 def main():
-    criket= []
+    cricket= []
     badminton = []
     football = []
 
     for _ in range(int(input("How many want to play cricket: "))):
-        criket.append(int(input("Enter Roll numbers: ")))
+        cricket.append(int(input("Enter Roll numbers: ")))
         
     for _ in range(int(input("How many want to play batminton: "))):
         badminton.append(int(input("Enter Roll numbers: ")))
@@ -41,10 +40,10 @@ def main():
     for _ in range(int(input("How many want to play football: "))):
         football.append(int(input("Enter Roll numbers: ")))
 
-    print(f"Play's both Cricket and Badminton:{intersection(criket, badminton)} ")
-    print(f"Play's Cricket or batminton but not both:{eiter_or(criket, badminton)} ")
-    print(f"Play's neitner cricket nor football:{minus(minus(badminton, criket), football)} ")
-    print(f"Cricket and football but not batminton:{minus(intersection(criket, football), badminton)} ")
+    print(f" Play's both Cricket and Badminton: {intersection(cricket, badminton)} ")
+    print(f" Play's Cricket or Badminton but not both: {neither_or(cricket, badminton)} ")
+    print(f" Play's neither Cricket nor Badminton: {minus(minus(badminton, cricket), football)} ")
+    print(f" Cricket and Football but not Badminton: {minus(intersection(cricket, football), badminton)} ")
 
 main()
  
