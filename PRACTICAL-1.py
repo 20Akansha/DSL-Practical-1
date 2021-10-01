ee21be27256dbf2bd93ba68dc2462968d44a84f7
Akansha Zutshi <90608726+20Akansha@users.noreply.github.com>
2021-10-01 22:04:29 +0530

    Update PRACTICAL-1.py

--- PRACTICAL-1.py
+++ PRACTICAL-1.py
@@ -1,71 +1,72 @@
-n=int(input("Enter the total number of students : "))                                        #practical 1
-s=[]
+n=int(input("Enter the total number of students in class: "))                             #practical 1
+FDS=[]
 for i in range(n):
-    marks=int(input(f"enter marks of {i+1} :"))
-    s.append(marks)
-print("the list of marks of student is ",s)
+    marks=int(input(f"Enter marks of student {i+1} : "))
+    FDS.append(marks)
+print("The list of marks of students is ",FDS)
 
 
 def average(num):
-    sum = 0
+    sum=0
     total=0
     for i in num:
-        sum +=i
+        sum+=i
         total+=1
-
-    avg = sum / total
+    avg=sum/total
     return avg
-avg=average(s)
-print("the average of students is",avg)
+
+avg=average(FDS)
+print("The average of marks is:",avg)
 
 
-def max():
-      
-         max_value = None
-         if i !=-999:
-              for num in s:
-                 if (max_value is None or num > max_value):
-                    max_value = num
-         print('The max marks is:', max_value)
+def maximum():
+    max_value=None
+    if i!=-1:
+        for num in FDS:
+            if max_value is None or num>max_value:
+                max_value=num
+        print("The maximum marks are: ",max_value)
+maximum()        
+        
 
-         
-def min():
-      
-         mini = s[0]
-         for i in range(len(s)):
-                if s[i] < mini:
-                    mini = s[i] 
-         print("The least marks is ",mini)
+def minimum():
+    min_value=None
+    for num in FDS:
+        num!=-1
+        if min_value is None or num<min_value:
+            if num>-1:
+                min_value=num
+    print("The minimum marks are:",min_value)
+    
+minimum()            
 
 
 def frequency():
-        max = 0
-        res = s[0]
-        total=0
+    max=0
+    list=FDS[0]
+    total=0
+    for i in FDS:       
+        for j in range(1,len(FDS)):
+            if FDS[j]==FDS[j-1]:
+                total+=1
+                if total>max:
+                    max=total
+                    list=FDS[j-1]
+        return list
     
-        for i in s:
-            for j in range(1,len(s)):
-                if (s[j] == s[j - 1]):
-                    total+=1
-                    if total> max:
-                        max = total
-                        res = s[j-1]
-            return res
-print("The most frequent marks is",frequency())            
+print("The most frequently occuring marks are",frequency())
 
-
-def absent():
-            
-            ab=0
-            for i in s:
-                if i ==-999:
-                     ab+=1
-                else:
-                    pass
+              
                     
-            print("The number of absent students are",ab)
-     
-            
-max()
-min()
-absent()
+def absent():
+    AB=0
+    for i in FDS:
+        if i == -1:
+            AB+=1
+        else:
+            pass
+    print("The number of absent students is : ",AB)
+
+    
+absent()        
+ 
